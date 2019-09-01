22a73e20c3874d446ae1e237888640d4c2f54ad3
iosetek <ignacy.osetek@gmail.com>
2019-09-02 00:50:35 +0200

    Fix and simplify Config class and tests

--- src/config.py
+++ src/config.py
@@ -4,8 +4,8 @@ import yaml
 class Config:
     def __init__(self, input_file):
         c = self.__load_config_file(input_file)
-        self.__check_if_keys_exist(c)
-        self.__validate_value_types(c)
+        self.__validate_types(c)
+        self.__custom_validations(c)
         self.__save_configuration(c)
         
 
@@ -18,52 +18,25 @@ class Config:
         return c
 
     
-    def __check_if_keys_exist(self, c):
+    def __validate_types(self, c):
         if self.__CONFIG not in c:
-            raise Config.MissingConfigException
-        if self.__TRAINING_RECORDS not in c[self.__CONFIG]:
-            raise Config.MissingTrainingRecordsException
-        if self.__TEST_RECORDS not in c[self.__CONFIG]:
-            raise Config.MissingTestRecordsException
-        if self.__EM_REPEATS not in c[self.__CONFIG]:
-            raise Config.MissingEMRepeatsException
-        if self.__DISCRETIZATION_STEP not in c[self.__CONFIG]:
-            raise Config.MissingDiscretizationStepException
-        if self.__GAUSSIANS_COUNT not in c[self.__CONFIG]:
-            raise Config.MissingGaussiansCountException
-        if self.__TRAINING_SUPERVECTORS not in c[self.__CONFIG]:
-            raise Config.MissingTrainingSupervectorsException
-        if self.__TEST_SUPERVECTORS not in c[self.__CONFIG]:
-            raise Config.MissingTestSupervectorsException
-        if self.__START_FROM not in c[self.__CONFIG]:
-            raise Config.MissingStartFromException
+            raise Config.MissingParametersException
+        for par in iter(self.__get_parameters()):
+            if par[Config.__VALUE] not in c[self.__CONFIG]:
+                raise Config.MissingParametersException
+            if not isinstance(c[self.__CONFIG][par[Config.__VALUE]], par[Config.__TYPE]):
+                raise Config.InvalidParametersException
 
 
-    def __validate_value_types(self, c):
-        if not isinstance(c[self.__CONFIG][self.__TRAINING_RECORDS], list):
-            raise Config.TrainingPathsFieldIsNotListException
-        if not isinstance(c[self.__CONFIG][self.__TEST_RECORDS], list):
-            raise Config.TestPathsFieldIsNotListException
-        if not isinstance(c[self.__CONFIG][self.__EM_REPEATS], int):
-            raise Config.EMRepeatsFieldIsNotIntigerException
-        if not isinstance(c[self.__CONFIG][self.__DISCRETIZATION_STEP], int):
-            raise Config.DiscretizationStepIsNotIntigerException
-        if not isinstance(c[self.__CONFIG][self.__GAUSSIANS_COUNT], int):
-            raise Config.GaussiansCountIsNotIntigerException
-        if not isinstance(c[self.__CONFIG][self.__TRAINING_SUPERVECTORS], str):
-            raise Config.TrainingSupervectorsFieldIsNotStringException
-        if not isinstance(c[self.__CONFIG][self.__TEST_SUPERVECTORS], str):
-            raise Config.TestSupervectorsFieldIsNotStringException
-        if not isinstance(c[self.__CONFIG][self.__START_FROM], str) and \
-            not self.__is_proper_start_from(c[self.__CONFIG][self.__START_FROM]):
-            raise Config.StartFromFieldIsNotValidException
+    def __custom_validations(self, c):
+        if not self.__is_proper_start_from(c[self.__CONFIG][self.__START_FROM]):
+            raise Config.InvalidParametersException
 
 
     def __is_proper_start_from(self, value):
-        if value == self.FROM_START and value == self.USE_READY_MFCC_BANK:
+        if value == self.FROM_START or value == self.USE_READY_MFCC_BANK:
             return True
         return False
-
 
 
     def __save_configuration(self, c):
@@ -77,7 +50,23 @@ class Config:
         self.start_from = c[self.__CONFIG][self.__START_FROM]
 
 
+    def __get_parameters(self):
+        return [
+            {Config.__VALUE:Config.__TRAINING_RECORDS, Config.__TYPE:list},
+            {Config.__VALUE:Config.__TEST_RECORDS, Config.__TYPE:list},
+            {Config.__VALUE:Config.__EM_REPEATS, Config.__TYPE:int},
+            {Config.__VALUE:Config.__DISCRETIZATION_STEP, Config.__TYPE:int},
+            {Config.__VALUE:Config.__GAUSSIANS_COUNT, Config.__TYPE:int},
+            {Config.__VALUE:Config.__TRAINING_SUPERVECTORS, Config.__TYPE:str},
+            {Config.__VALUE:Config.__TEST_SUPERVECTORS, Config.__TYPE:str},
+            {Config.__VALUE:Config.__START_FROM, Config.__TYPE:str}]
+
+
+    __VALUE = "val"
+    __TYPE = "type"
+
     __CONFIG = "config"
+
     __TRAINING_RECORDS = "training_records"
     __TEST_RECORDS = "test_records"
     __EM_REPEATS = "em_repeats"
@@ -92,74 +81,13 @@ class Config:
     USE_READY_MFCC_BANK = "USE_READY_MFCC_BANK"
 
 
-
-    class MissingTestRecordsException(Exception):
-        pass
-
-
-    class MissingTrainingRecordsException(Exception):
-        pass
-
-
-    class MissingEMRepeatsException(Exception):
-        pass
-
-
-    class MissingConfigException(Exception):
-        pass
-
-
-    class MissingDiscretizationStepException(Exception):
-        pass
-
-
-    class MissingGaussiansCountException(Exception):
-        pass
-
-
-    class MissingTrainingSupervectorsException(Exception):
-        pass
-
-
-    class MissingTestSupervectorsException(Exception):
-        pass
-
-
-    class MissingStartFromException(Exception):
-        pass
-
-
     class InvalidYamlFileException(Exception):
         pass
 
 
-    class TrainingPathsFieldIsNotListException(Exception):
+    class MissingParametersException(Exception):
         pass
 
 
-    class TestPathsFieldIsNotListException(Exception):
-        pass
-
-
-    class EMRepeatsFieldIsNotIntigerException(Exception):
-        pass
-
-
-    class DiscretizationStepIsNotIntigerException(Exception):
-        pass
-
-
-    class GaussiansCountIsNotIntigerException(Exception):
-        pass
-
-
-    class TrainingSupervectorsFieldIsNotStringException(Exception):
-        pass
-
-
-    class TestSupervectorsFieldIsNotStringException(Exception):
-        pass
-
-
-    class StartFromFieldIsNotValidException(Exception):
+    class InvalidParametersException(Exception):
         pass
--- src/tests/test_config.py
+++ src/tests/test_config.py
@@ -7,50 +7,25 @@ def path_to(file):
 
 
 class TestConfig(unittest.TestCase):
-    def test_config_without_training_records(self):
-        with self.assertRaises(Config.MissingTrainingRecordsException):
-            Config(path_to("test_config_without_training_records.yml"))
-
-    
-    def test_config_without_test_records(self):
-        with self.assertRaises(Config.MissingTestRecordsException):
-            Config(path_to("test_config_without_test_records.yml"))
-
-    
-    def test_config_without_em_repeats(self):
-        with self.assertRaises(Config.MissingEMRepeatsException):
-            Config(path_to("test_config_without_em_repeats.yml"))
-
-    
-    def test_invalid_yaml_structure(self):
+    def test_invalid_file_form(self):
         with self.assertRaises(Config.InvalidYamlFileException):
             Config(path_to("test_config_with_invalid_structure.yml"))
 
 
-    def test_config_without_config(self):
-        with self.assertRaises(Config.MissingConfigException):
-            Config(path_to("test_config_without_config.yml"))
+    def test_missing_field(self):
+        with self.assertRaises(Config.MissingParametersException):
+            Config(path_to("test_config_missing_parameter.yml"))
 
 
-    def test_config_invalid_training_records_form(self):
-        with self.assertRaises(Config.TrainingPathsFieldIsNotListException):
-            Config(path_to("test_config_invalid_training_records_form.yml"))
-
-
-    def test_config_invalid_test_records_form(self):
-        with self.assertRaises(Config.TestPathsFieldIsNotListException):
-            Config(path_to("test_config_invalid_test_records_form.yml"))
-
-
-    def test_config_invalid_em_repeats_form(self):
-        with self.assertRaises(Config.EMRepeatsFieldIsNotIntigerException):
-            Config(path_to("test_config_invalid_em_repeats_form.yml"))
+    def test_invalid_field(self):
+        with self.assertRaises(Config.InvalidParametersException):
+            Config(path_to("test_config_invalid_parameter.yml"))
 
 
     def test_reading_config(self):
-        c = Config("src/tests/testdata/test_loading_config/test_config.yml")
-        self.assertEqual(c.training_paths, ["data/commands/tracks", "data/commands/tracks-ania"])
-        self.assertEqual(c.test_paths, ["data/commands/test-records"])
+        c = Config(path_to("test_config.yml"))
+        self.assertEqual(c.training_paths, ["training_path_a", "training_path_b"])
+        self.assertEqual(c.test_paths, ["test_path"])
         self.assertEqual(c.em_repeats, 20)
         self.assertEqual(c.discretization_step, 50)
         self.assertEqual(c.gaussians_count, 2)
